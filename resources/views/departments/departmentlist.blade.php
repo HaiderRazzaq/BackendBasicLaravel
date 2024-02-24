7147f3123edca83aed2e0e0f4ee4438b9a5943fd
Haider Razzaq <haiderhac@gmail.com>
2024-02-24 15:44:43 +0300

    department list view

--- resources/views/departments/departmentlist.blade.php
+++ resources/views/departments/departmentlist.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+@extends('layout.app')
+
+@section('title')
+    Departments list
+@endsection
+
+@section('departments list')
+    <div class="container mt-5">
+        <h2>Departments List</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Department Name</th>
+                    <th scope="col">Owner</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td id="departmentName">Sample Department</td>
+                    <td id="owner">John Doe</td>
+                    <td>
+                        <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                        <button type="button" class="btn btn-info btn-sm">Show</button>
+                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
