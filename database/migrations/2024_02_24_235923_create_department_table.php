4ce67ba68dc1a901f0a836d17eee281fd40595c5
Haider Razzaq <haiderhac@gmail.com>
2024-02-25 03:01:29 +0300

    create department table

--- database/migrations/2024_02_24_235923_create_department_table.php
+++ database/migrations/2024_02_24_235923_create_department_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('classrooms', function (Blueprint $table) {
+        Schema::create('department', function (Blueprint $table) {
             $table->id();
-            $table->string('class_name');
-            $table->string('class_owner');
+            $table->string('department_name');
+            $table->string('department_owner');
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('classrooms');
+        Schema::dropIfExists('department');
     }
 };
