ffb60a1b9dea8f353c99a74f8deae8dda2271c13
Haider Razzaq <haiderhac@gmail.com>
2024-03-06 14:13:42 +0300

    edit blade page for department done

--- resources/views/departments/editdepartment.blade.php
+++ resources/views/departments/editdepartment.blade.php
@@ -8,17 +8,19 @@
 @section('editdepartment')
 
 
-<form class="row g-3 w-a inputform">
+<form class="row g-3 w-a inputform" method="POST" action="{{route('departments.update',$department->id)}}">
+    @csrf
+    @method('PUT')
     <div class="col-md-6 mb-3">
         <label for="inputDepartment" class="form-label">Edit Department Name</label>
-        <input type="text" class="form-control" id="inputDepartment" required>
+        <input type="text" class="form-control" value="{{$department->department_name}}" id="inputDepartment" required>
     </div>
     <div class="col-md-6 mb-3">
         <label for="inputOwner" class="form-label">Edit Owner</label>
-        <input type="text" class="form-control" id="inputOwner" required>
+        <input type="text" class="form-control" value="{{$department->department_owner}}" id="inputOwner" required>
     </div>
     <div class="col-12">
-        <button type="submit" class="btn btn-primary">submit</button>
+        <button type="submit" value="submit" class="btn btn-primary">submit</button>
     </div>
 </form>
 
