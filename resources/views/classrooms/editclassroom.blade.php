47cf2dbde3e3bff1383f25fc9cd31ff64772bc18
Haider Razzaq <haiderhac@gmail.com>
2024-03-06 17:57:45 +0300

    edit blade page for classrooms

--- resources/views/classrooms/editclassroom.blade.php
+++ resources/views/classrooms/editclassroom.blade.php
@@ -3,22 +3,23 @@
 
 
 @section('title')
-Edit classroom
+    Edit classroom
 @endsection
 
 @section('editclassroom')
-<form class="row g-3 w-a inputform">
-    <div class="col-md-6 mb-3">
-        <label for="inputclass" class="form-label">Edit Class Name</label>
-        <input type="text" class="form-control" id="inputclass" required>
-    </div>
-    <div class="col-md-6 mb-3">
-        <label for="inputOwner" class="form-label">Edit Owner</label>
-        <input type="text" class="form-control" id="inputOwner" required>
-    </div>
-    <div class="col-12">
-        <button type="submit" class="btn btn-primary">submit</button>
-    </div>
-</form>
-
+    <form class="row g-3 w-a inputform" method="POST" action="{{ route('classrooms.update', 2) }}">
+        @csrf
+        @method('PUT')
+        <div class="col-md-6 mb-3">
+            <label for="inputclass" class="form-label">Edit Class Name</label>
+            <input type="text" class="form-control" name="classname" id="inputclass" required>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label for="inputOwner" class="form-label">Edit Owner</label>
+            <input type="text" class="form-control" name="classowner" id="inputOwner" required>
+        </div>
+        <div class="col-12">
+            <button type="submit" value="submit" class="btn btn-primary">update</button>
+        </div>
+    </form>
 @endsection
