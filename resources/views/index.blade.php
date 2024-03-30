09a7afe12e62e96cef26d811710be9bab4dbc0b4
Haider Razzaq <haiderhac@gmail.com>
2024-03-30 03:56:53 +0300

    middleware

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,4 +8,5 @@
 @section('index')
 @endsection --}}
 
+@dd(Auth::user());
 <x-layout></x-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/welcome', function () {
+Route::middleware('auth')->get('/welcome', function () {
     return view('welcome');
 });
 
